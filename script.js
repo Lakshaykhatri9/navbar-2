1c17bd7b23dbbe37166e9ffe1125b055a08d8c55
LakshayKhatri9 <lk2417684@gmail.com>
2024-09-06 23:15:13 +0530

    navbar completed

--- script.js
+++ script.js
@@ -5,3 +5,37 @@ function toggleNav() {
     // Toggle the active class on the navbar to open/close it
     document.getElementById("navbar").classList.toggle("active");
 }
+
+
+function toggleAnswer(element) {
+    // Find the parent element (faq-item) and toggle the 'active' class
+    const faqItem = element.parentElement;
+    faqItem.classList.toggle("active");
+
+    // Get the answer div
+    const answer = faqItem.querySelector(".faq-answer");
+
+    // Toggle the max-height for smooth transition
+    if (faqItem.classList.contains("active")) {
+        answer.style.maxHeight = answer.scrollHeight + "px"; // Expand
+    } else {
+        answer.style.maxHeight = 0; // Collapse
+    }
+}
+
+
+let table = document.querySelector(".table-1");
+let ongoing = document.querySelector(".ongoing");
+let resi = document.querySelector(".resi");
+let del = document.querySelector(".del");
+let table_2 = document.querySelector(".table-2")
+
+ongoing.addEventListener("click", () => {
+    table.classList.toggle("table-1-hide");
+    table_2.classList.add("table-2-hide");
+});
+
+del.addEventListener("click",() => {
+   table_2.classList.toggle("table-2-hide");
+   table.classList.add("table-1-hide");
+});
