8706230be83b2f94eac7fce06042e158d75c9cd4
LakshayKhatri9 <lk2417684@gmail.com>
2024-09-06 20:14:47 +0530

    navbar 2

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleNav() {
+    // Toggle the change class on the hamburger menu (for transforming to cross)
+    document.getElementById("hamburger").classList.toggle("change");
+
+    // Toggle the active class on the navbar to open/close it
+    document.getElementById("navbar").classList.toggle("active");
+}
